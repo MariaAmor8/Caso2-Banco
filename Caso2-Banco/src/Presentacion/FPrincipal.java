2ea7c9f411252d299ae1e60ef586f71266885bd5
María José Amorocho <123780212+MariaAmor8@users.noreply.github.com>
2023-11-29 08:42:43 -0500

    gui creada

--- Caso2-Banco/src/Presentacion/FPrincipal.java
+++ Caso2-Banco/src/Presentacion/FPrincipal.java
@@ -1,9 +1,52 @@
 package Presentacion;
 
-public class FPrincipal {
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class FPrincipal extends JFrame{
+	JTextField txtNumero;
+	JTextField txtMonto;
+	
+	public void ejecutar() {
+		this.setPreferredSize(new Dimension(350,250));
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setVisible(true);
+		this.setTitle("Banco");
+		this.setLocationRelativeTo(null);
+		
+		this.setLayout(new GridLayout(3,2));
+		this.add(new JLabel("Numero de cuenta"));
+		this.txtNumero = new JTextField();
+		this.add(this.txtNumero);
+		this.add(new JLabel("Monto"));
+		this.txtMonto = new JTextField();
+		this.add(this.txtMonto);
+		
+		JButton bEnviar = new JButton("Enviar");
+		this.add(bEnviar);
+		bEnviar.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
+		this.pack();
+	}
 
 	public static void main(String[] args) {
-		System.out.println("hello");
+		
+		FPrincipal principal = new FPrincipal();
+		principal.ejecutar();
 	}
 
 }
